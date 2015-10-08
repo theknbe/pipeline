ae84a1299c2b8f6b37b007f285377fa4e00f11f0
theknbe <knhbyrne@gmail.com>
2015-10-08 12:46:09 -0700

    added mrmClose function, updated kb_macCreateMesh

--- silverVista/kb_macCreateMesh.m
+++ silverVista/kb_macCreateMesh.m
@@ -1,11 +1,27 @@
-projHome = sprintf('/Volumes/passportKB/DATA/new');
-subj = sprintf('MG_050414');
+% kb_initializeVista.m
+% 
+% Kelly Byrne | 09.02.15
+%
+% builds and saves a smoothed and inflated mesh for each hemispheres
+%
+% required input: user-defined parameters
+% desired output: lh_pial.mat, rh_pial.mat, lh_inflated.mat rh_inflated.mat
+% ________________________________________________________________________________________________
+
+% user-defined parameters:
+projHome = sprintf('/Volumes/passportKB/DATA');
+subj = sprintf('LL_100115');
+
+% open a connection to the mesh server
+% GUI will appear asking to allow incoming connections, this is OK - allow it!
+% to stop the GUI from appearing, add a signature to the mrMeshMac application and server executable
+% see the website below (specifically answers 2 & 3) for helpful information about this process 
+% http://apple.stackexchange.com/questions/3271/how-to-get-rid-of-firewall-accept-incoming-connections-dialog
+close all;
+mrmStart(1,'localhost');
 
 % build left hemisphere mesh
-close all;
-windowID = mrmStart(1, 'localhost');
-
-fName = fullfile(projHome, filesep, subj, 'nifti/t1_class.nii.gz');
+fName = fullfile(projHome, filesep, subj, 'nifti/t1Class.nii.gz');
 msh = meshBuildFromNiftiClass(fName, 'left');
 msh = meshSmooth(msh);
 msh = meshColor(msh);
@@ -14,8 +30,7 @@ save lh_pial.mat msh
 clear msh
 
 % build right hemisphere mesh 
-
-fName = fullfile(projHome, filesep, subj, 'nifti/t1_class.nii.gz');
+fName = fullfile(projHome, filesep, subj, 'nifti/t1Class.nii.gz');
 msh = meshBuildFromNiftiClass(fName, 'right');
 msh = meshSmooth(msh);
 msh = meshColor(msh);
@@ -24,25 +39,30 @@ save rh_pial.mat msh
 clear msh
 
 % inflate and display LH
-
 load lh_pial.mat
 msh = meshSet(msh,'smooth_iterations',600);
 msh = meshSet(msh,'smooth_relaxation',0.5);
 msh = meshSet(msh,'smooth_sinc_method',0);
 msh = meshSmooth(msh);
-meshVisualize(msh);
 
 save lh_inflated.mat msh
 clear msh
 
 % inflate and display RH
-
 load rh_pial.mat
 msh = meshSet(msh,'smooth_iterations',600);
 msh = meshSet(msh,'smooth_relaxation',0.5);
 msh = meshSet(msh,'smooth_sinc_method',0);
 msh = meshSmooth(msh);
-meshVisualize(msh);
 
 save rh_inflated.mat msh
 clear msh
+
+% close windows and connection to the mesh server
+mrmCloseWindow(1001,'localhost');
+mrmCloseWindow(1003,'localhost');
+mrmCloseWindow(1005,'localhost');
+mrmCloseWindow(1007,'localhost');
+PATH = getenv('PATH');
+setenv('PATH', [PATH ':/usr/local/bin']);
+unix('kb_mrmClose.sh');
