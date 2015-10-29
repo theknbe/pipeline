ce3b159b124b69186c28fe9cfdf689e2ae00edd3
theknbe <knhbyrne@gmail.com>
2015-10-29 16:00:28 -0700

    added header

--- silverVista/kb_installSeg.m
+++ silverVista/kb_installSeg.m
@@ -1,6 +1,19 @@
-% close open windows and open mrVista
-clear all;
-close all;
+% kb_installSeg.m
+% 
+% Kelly Byrne | Silver Lab | UC Berkeley | 
+% modified from code written by the Winawer lab and available at: https://wikis.nyu.edu/display/winawerlab/Initialize+data
+%
+% requires the VISTA Lab's Vistasoft package - available at: https://github.com/vistalab/vistasoft
+%
+% initiates an mrVista session for the given subject
+%
+% required input: user-defined parameters
+% desired output: mrInit2_params.mat, mrSESSION.mat, mrSESSION_backup.mat
+% ________________________________________________________________________________________________
+
+
+% close any open windows and open mrVista
+clear all; close all;
 mrVista;
 
 % install segmentation
